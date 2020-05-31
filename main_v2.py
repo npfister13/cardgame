0bd873d29490963e5b2ef3c84bcf523959f537d1
npfister13 <32338695+npfister13@users.noreply.github.com>
2020-05-31 18:14:53 -0400

    attemps to make combat
    not going well

--- main_v2.py
+++ main_v2.py
@@ -16,27 +16,64 @@ def main():
     # print("Your hand: ")
     # print(len(user_hand))
     # for i in range(len(user_hand)):
-    #     print(user_hand[i]['Name'])
-    #     print("[{0}, {1}]".format(user_hand[i]['HP'], user_hand[i]['STR']))
+    #     print(user_hand[i]['name'])
+    #     print("[{0}, {1}]".format(user_hand[i]['hp'], user_hand[i]['str']))
 
     print_board(user_hand, opponent_hand, user_hp, opponent_hp)
+    input("\nPress anything to continue.")
     # TODO: figure out how to handle combat
+    combat(user_hand, opponent_hand, user_hp, opponent_hp)
+
+
+def combat(user_hand, opponent_hand, user_hp, opponent_hp):
+    print_board(user_hand, opponent_hand, user_hp, opponent_hp)
+    print("\n\tBattle start!")
+    whose_turn = determine_first()
+    # monsters attack from left to right, so these will count up to the
+    # last card in their hand
+    op_monster_to_attack = 0
+    us_monster_to_attack = 0
+    if whose_turn == "o":
+        print("\tOpponent goes first.")
+        monster_attacking(user_hand, opponent_hand, us_monster_to_attack,
+                          op_monster_to_attack, whose_turn)
+    else:
+        print("\tYou go first.")
+
+
+# this handles calculations for when one monster attacks another
+def monster_attacking(user_hand, opponent_hand, us_monster_to_attack,
+                      op_monster_to_attack, whose_turn):
+    if whose_turn == "o":
+        # grabs the monster from the opponents hand
+        op_monster = opponent_hand[op_monster_to_attack]
+        # chooses a random card to attack
+        us_monster = user_hand[random.randint(0, len(user_hand))]
+        # opponent_hand[op_monster['name']]['hp'] = (opponent_hand[op_monster]['hp'] - us_monster['str'])
+        # user_hand[us_monster['name']]['hp'] = (user_hand[us_monster['hp']]) - op_monster['str']
+        print("{0} attacks {1}!".format(op_monster['name'], us_monster['name']))
+
+
+def determine_first():
+    # return random.choice(["o", "u"])
+    # TODO: change this back once opponent and user are finished
+    return "o"
 
 
 # prints out the board, duh
 def print_board(user_hand, opponent_hand, user_hp, opponent_hp):
     for i in range(15):
         print("")
-    print("Opponent HP: {}".format(opponent_hp).center(30))
+    print("Opponent hp: {}".format(opponent_hp).center(30))
     for i in range(len(opponent_hand)):
-        print("[{}]".format(opponent_hand[i]['Name']).center(15), end="")
+        print("[{}]".format(opponent_hand[i]['name']).center(15), end="")
     print()
     print_hand(opponent_hand)
     print()
     print("-"*30)
-    print("Your HP: {}".format(user_hp).center(30))
+    print("Your hp: {}".format(user_hp).center(30))
     for i in range(len(user_hand)):
-        print("[{}]".format(user_hand[i]['Name']).center(15), end="")
+        print("[{}]".format(user_hand[i]['name']).center(15), end="")
     print()
     print_hand(user_hand)
 
@@ -44,8 +81,8 @@ def print_board(user_hand, opponent_hand, user_hp, opponent_hp):
 # prints the hand given to it
 def print_hand(hand):
     for i in range(len(hand)):
-        hp = hand[i]['HP']
-        strength = hand[i]['STR']
+        hp = hand[i]['hp']
+        strength = hand[i]['str']
         print("[{0}, {1}] ".format(hp, strength).center(15), end="")
 
 
@@ -57,7 +94,7 @@ def add_card(user_hand, opponent_hand, monsters_dict):
     print("Opponent hand: ", opponent_hand)
 
 
-# contains HP of players
+# contains hp of players
 class Score:
     def __init__(self):
         self.user_hp = 5
