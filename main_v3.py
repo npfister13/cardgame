61ed644440ca9381223fa156bb92a41bcdc50b6b
npfister13 <32338695+npfister13@users.noreply.github.com>
2020-12-15 23:56:42 -0500

    combat updates
    combat will now consider the HP of the attacker and receiver. game will also end combat if hp of all monsters in one or both players hands reaches 0 or lower.

--- main_v3.py
+++ main_v3.py
@@ -4,9 +4,10 @@ import json
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, name):
         self.hp = 5
         self.hand = []
+        self.name = name
 
     def addCard(self, card):
         self.hand.append(card)
@@ -56,35 +57,104 @@ def printBoard(user, opponent):
     printHand(user)
 
 
-def determine_first():
+def determineFirst():
+    first = "o"
     # return random.choice(["o", "u"])
-    return "o"
+    if first == "o":
+        print("Opponent goes first!")
+        return "o"
+    else:
+        print("User goes first!")
+        return "u"
+
+
+# this runs to check the HP of all monsters in the player's hand to determine if to continue fighting or not
+def checkDeckHealth(player):
+    for card in range(len(player.hand)):
+        if player.hand[card]['hp'] > 0:
+            return True
+    return False
+
+
+def checkCardHealth(player, monster):
+    return player.hand[monster]['hp']
+
+# TODO 12/15 when you get back, make it to where the game continues running after first combat
+
+# is it possible to refer to the winner/loser as opponent/user or vice versa without making print statements
+# specifically for it?
+# nvm, i guess i could make a .name for the self init
+def combatEndMessage(winner, loser):
+    print("{0} wins! {1} takes one damage.".format(winner.name, loser.name))
+    loser.hp -= 1
+    print("{0} now has {1} hp".format(loser.name, loser.hp))
 
 
 def combat(user, opponent):
     printBoard(user, opponent)
     print("\nBattle start!")
-    whose_turn = determine_first()
-
+    whoseTurn = determineFirst()
+    userAlive = True
+    opponentAlive = True
     # monsters attack left to right
     # used to determine which monster is currently attacking
     userMonAtk = 0
     opponentMonAtk = 0
-    if whose_turn == "u":
-        print("User goes first!")
-        pass
-    elif whose_turn == "o":
-        print("Opponent goes first!")
-        randUserMon = random.randint(0, len(user.hand) - 1)
-        opMonster = opponent.hand[opponentMonAtk]
-        usMonster = user.hand[randUserMon]
-        # takes the user monster being attacked and reduces by opponent monster str
-        usMonster['hp'] -= opMonster['str']
-        # opponent monster attacking also loses hp based on str of monster it attacks
-        opMonster['hp'] -= usMonster['str']
-        printBoard(user, opponent)
-        print("Opponent's {0} attacks your {1}!".format(opMonster['name'], usMonster['name']))
-        #TODO: Where you left off: checking how the opponent monster attacks randomly. Need to figure out how to ignore dead monsters and continue a fight (or stop when necessary)
+    while userAlive is not False and opponentAlive is not False:
+        # TODO: turn the whole if/else statements into a function (or two) to reduce duplicate code
+        if whoseTurn == "u":
+            if userMonAtk > len(opponent.hand):
+                userMonAtk = 0
+            while checkCardHealth(user, userMonAtk) <= 0:
+                userMonAtk += 1
+            randOpponentMon = random.randint(0, len(user.hand) - 1)
+            while opponent.hand[randOpponentMon]['hp'] <= 0:
+                randOpponentMon = random.randint(0, len(user.hand) - 1)
+            usMonster = user.hand[userMonAtk]
+            opMonster = opponent.hand[randOpponentMon]
+            opMonster['hp'] -= usMonster['str']
+            usMonster['hp'] -= opMonster['str']
+            printBoard(user, opponent)
+            print("Your {0} attacks opponent's {1}!".format(usMonster['name'], opMonster['name']))
+            opponentAlive = checkDeckHealth(opponent)
+            userAlive = checkDeckHealth(user)
+            print("userAlive = {}".format(userAlive))
+            print("opponentAlive = {}".format(opponentAlive))
+            input("Press anything to continue.")
+            whoseTurn = "o"
+            userMonAtk += 1
+
+        elif whoseTurn == "o":
+            if opponentMonAtk > len(opponent.hand):
+                opponentMonAtk = 0
+            while checkDeckHealth(opponent) < 0:
+                opponentMonAtk += 1
+            randUserMon = random.randint(0, len(user.hand) - 1)
+            # if the chosen enemy monster is already dead, find a different one to attack
+            while opponent.hand[randUserMon]['hp'] <= 0:
+                randUserMon = random.randint(0, len(user.hand) - 1)
+            opMonster = opponent.hand[opponentMonAtk]
+            usMonster = user.hand[randUserMon]
+            # takes the user monster being attacked and reduces by opponent monster str
+            usMonster['hp'] -= opMonster['str']
+            # opponent monster attacking also loses hp based on str of monster it attacks
+            opMonster['hp'] -= usMonster['str']
+            printBoard(user, opponent)
+            print("Opponent's {0} attacks your {1}!".format(opMonster['name'], usMonster['name']))
+            userAlive = checkDeckHealth(user)
+            opponentAlive = checkDeckHealth(opponent)
+            print("userAlive = {}".format(userAlive))
+            print("opponentAlive = {}".format(opponentAlive))
+            input("Press anything to continue.")
+            whoseTurn = "u"
+            opponentMonAtk += 1
+    if userAlive is False and opponentAlive is False:
+        print("It's a draw!")
+    elif userAlive is False:
+        combatEndMessage(opponent, user)
+    else:
+        combatEndMessage(user, opponent)
+        # TODO: Where you left off: checking how the opponent monster attacks randomly. Need to figure out how to ignore dead monsters and continue a fight (or stop when necessary)
 
 
 def main():
@@ -95,8 +165,8 @@ def main():
         monsters = json.load(f)
         gameDeck = monsters
     cardsInUse = []
-    user = Player()
-    opponent = Player()
+    user = Player('User')
+    opponent = Player('Opponent')
     if not testing:
         for i in range(2):
             card = random.choice(gameDeck)
